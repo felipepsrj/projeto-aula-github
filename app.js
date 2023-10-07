75bbe2f4847273a98b2f2ff046ff3b02de3c4b33
Felipe <andrefelipefsilva@gmail.com>
2023-10-06 21:57:00 -0300

    deixando o jogo mais dificil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimete = 10;
+let numeroLimete = 20;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -32,7 +32,7 @@ function verificarChute() {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 20');
 }
 
 exibirMensagemInicial();
